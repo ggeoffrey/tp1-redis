ce8e4809225189fe2ed020d01790d9e00f8960ba
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2016-10-01 09:57:15 +0200

    Removed old files

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import redis.clients.jedis.Jedis;
-
-public class Main {
-
-    public static final void main(String[] args) {
-        Jedis conn = new Jedis("localhost", 6379);
-
-    }
-}
--- src/VoteStatus.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by geoffrey on 26/09/2016.
- */
-public enum VoteStatus {
-    CANNOT_VOTE_ANYMORE,
-    ALREADY_VOTED,
-    VOTED,
-    UNDEFINED_STATE
-}
